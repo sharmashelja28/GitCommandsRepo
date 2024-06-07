5049dc3c34dacdb110d73c946e1825e1cc349e08
Shelja Sharma <sheljasharma@pixelsoftwares.co>
2024-06-07 18:08:17 +0530

    PageOne added

---
+++ src/PageOne.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const PageOne = () => {
+  return (
+    <View>
+      <Text>PageOne</Text>
+    </View>
+  )
+}
+
+export default PageOne
