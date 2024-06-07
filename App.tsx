d4b08daa6f8f91dc0acd7a6a6f93927579ce2e61
Shelja Sharma <sheljasharma@pixelsoftwares.co>
2024-06-07 17:30:52 +0530

    Remove Line in FirstScreen branch

--- App.tsx
+++ App.tsx
@@ -91,6 +91,7 @@ function App(): React.JSX.Element {
         <Text> SecondBrnach ---- Changes in file</Text>
         <Text> SecondBrnach ---- Changes in file</Text>
         <Text> SecondBrnach ---- Changes in file</Text>
+        {/* Remove the Line from here */}
      </View>
    
   );
