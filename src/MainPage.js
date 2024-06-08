6c940f22bb8e6e6a522bbdbb25a5facb5e029d4c
Shelja Sharma <sheljasharma@pixelsoftwares.co>
2024-06-08 10:54:56 +0530

    Second Page (by mistake wrong)

--- src/MainPage.js
+++ src/MainPage.js
@@ -5,7 +5,7 @@ export default function MainPage() {
   return (
     <View>
       <Text>MainPage</Text>
-      <Text>Main Page second line value is this</Text>
+      <Text>Second Page  line value is this</Text>
     </View>
   )
 }
